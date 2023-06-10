4d3e21c8b26aaf69860d92eeb7d4a74b14b1143f
pavel-else <de.pavel.se@yandex.ru>
2023-06-10 06:08:12 +0300

    added retryer

--- src/parse.js
+++ src/parse.js
@@ -1,5 +1,15 @@
 const { fetcher } = require('../fetcher.js');
 
+const retryedFetch = async (route) => {
+    const result = await fetcher(route);
+
+    if (result.status === 500) {
+        return fetcher(route);
+    }
+
+    return result;
+};
+
 const parse = async (url) => {
     const visitedRoutes = new Set();
     const routes = [url];
@@ -12,17 +22,23 @@ const parse = async (url) => {
         visitedRoutes.add(route);
 
         try {
-            const { status, text } = await fetcher(route);
+            const { status, text } = await retryedFetch(route);
 
             if (status === 200) {
                 const html = await text();
 
                 const hrefs = html.match(/href="([^\'\"]+)/g);
+
+                if (!hrefs) {
+                    continue;
+                }
+
                 hrefs.forEach((href) => {
                     routes.push(href.slice(6));
                 })
             }
         } catch (err) {
+            console.error(err);
         }
     }
 
